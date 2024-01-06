94f485409df873efd8b11649c1d2f29a9b9c3dbd
Roi <mailtoksy@gmail.com>
2024-01-06 19:46:03 +0900

    add component

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div className="text-5xl">
+        <h1>Header</h1>
+      </div>
+    );
+  };
+  
+  export default Header;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,14 @@
+import Header from "../components/header";
+
 const MainPage = () => {
   return (
+    <>
+    <Header/>
     <div className="text-3xl">
       <h1>Main Page</h1>
       <p>메인 페이지</p>
     </div>
+    </>
   );
 };
 
